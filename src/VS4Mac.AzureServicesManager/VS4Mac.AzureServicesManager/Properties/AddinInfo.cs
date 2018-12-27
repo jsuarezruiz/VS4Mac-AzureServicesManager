04b49039008c7f5ac49a2d613e265fe02d115c56
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-12-27 12:25:26 +0100

    Initial code

---
+++ src/VS4Mac.AzureServicesManager/VS4Mac.AzureServicesManager/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "VS4Mac.AzureServicesManager",
+    Namespace = "VS4Mac.AzureServicesManager",
+    Version = "0.1"
+)]
+
+[assembly: AddinName("VS4Mac.AzureServicesManager")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("VS4Mac.AzureServicesManager")]
+[assembly: AddinAuthor("Javier Suárez Ruiz")]
